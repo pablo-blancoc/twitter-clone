ab01109560c5ca84de96959238858883e4ef853c
Pablo Blanco <pabloblanco@fb.com>
2021-06-28 18:45:37 -0500

    TweetsAdapter not finished
    In CodePath's tutorial, I left on minute 6:45 of Connecting the Adapter. Adapter almost done, missing part of Bindind data to the specific ViewHolder.

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,89 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    // Atrributes
+    private Context context;
+    private List<Tweet> tweets;
+
+    /**
+     * Constructor for the TweetsAdapter
+     * @param context: The context to bind it to
+     * @param tweets: The list of tweets to show. The data it contains
+     */
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    /**
+     * For each tweet, inflate a layout
+     * @param parent: The parent ViewGroup to inflate the layout to
+     * @param viewType: Type of view to inflate
+     * @return TweetsAdapter.ViewHolder
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    /**
+     * Returns the number of tweets
+     * @return int
+     */
+    @Override
+    public int getItemCount() {
+        return this.tweets.size();
+    }
+
+
+    /**
+     * Class for TweetsAdapter ViewHolder
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Attributes
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+
+        /**
+         * Constructor for the ViewHolder
+         * @param itemView: A View of a tweet
+         */
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // Find references on .xml
+            this.ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            this.tvBody = itemView.findViewById(R.id.tvBody);
+            this.tvScreenName = itemView.findViewById(R.id.tvScreenName);
+
+
+        }
+    }
+
+}
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,7 +1,13 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.util.Log;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tweet {
 
@@ -10,12 +16,31 @@ public class Tweet {
     public String createdAt;
     public User user;
 
+    /**
+     * Create a Tweet from a JSONObject that Twitter API responded
+     * @param jsonObject: The JSONObject to turn to a Tweet
+     * @return Tweet
+     * @throws JSONException: Key not found on object
+     */
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
         return tweet;
+    }
+
+    /**
+     * Creates a list of tweets out of a list of JSONObjects that Twitter's API responded
+     * @param jsonArray: The JSONArray to turn into a list of Tweets
+     * @return: List<Tweet>
+     */
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for(int i=0; i< jsonArray.length(); i++) {
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return tweets;
     }
 
 }
